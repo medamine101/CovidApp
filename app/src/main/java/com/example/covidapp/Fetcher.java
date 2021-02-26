d5e074e758e0ff265570f9535a303def0c4cc9c3
Mohamed Amine Smires <mohamedamines@outlook.com>
2021-02-26 11:17:31 -0500

    Adding call to API
    Basic call to API included in commit. API call is located in fragment class, but to be moved later to a separate class.

---
+++ app/src/main/java/com/example/covidapp/Fetcher.java
@@ -0,0 +1,27 @@
+package com.example.covidapp;
+
+import android.app.DownloadManager;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import java.lang.ref.ReferenceQueue;
+
+public class Fetcher {
+
+
+    public void getData(){
+
+       // RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
