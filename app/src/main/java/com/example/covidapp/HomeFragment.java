afafdf54abfee660188b924b0064d009b0909706
k3vl <kevin08904@gmail.com>
2021-04-03 14:13:34 -0400

    first

--- app/src/main/java/com/example/covidapp/HomeFragment.java
+++ app/src/main/java/com/example/covidapp/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.covidapp;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +15,93 @@ import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+    private final QuestionLibrary questionLibrary = new QuestionLibrary();
 
+    private TextView questionView;
+    private Button buttonChoice1;
+    private Button buttonChoice2;
+
+    private String answer;
+    private int score = 0;
+    private int questionNumber = 0;
+    private double percentage;
+
+    @SuppressLint("DefaultLocale")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate( R.layout.fragment_home, container, false );
+
+        questionView = view.findViewById( R.id.questions );
+        buttonChoice1 = view.findViewById( R.id.choice1 );
+        buttonChoice2 = view.findViewById( R.id.choice2 );
+
+        updateQuestion();
+
+        // after click on the first button
+        buttonChoice1.setOnClickListener( view1 -> {
+            if (buttonChoice1.getText() == "Try Again") {
+                score = 0;
+                questionNumber = 0;
+                buttonChoice2.setVisibility( View.VISIBLE );
+                questionView.setText( questionLibrary.getQuestion( questionNumber ) );
+                buttonChoice1.setText( questionLibrary.getChoice1( questionNumber ) );
+                buttonChoice2.setText( questionLibrary.getChoice2( questionNumber ) );
+            }
+            if (buttonChoice1.getText() == answer) {
+                score = score + 1;
+            }
+            if (questionNumber == questionLibrary.getLength()) {
+                if(score-1 < 3){
+                    updateScore( "low" );
+                }
+                else if (score-1 > 5){
+                    updateScore( "high" );
+                }
+                else {
+                    updateScore( "medium" );
+                }
+            }
+            if (questionNumber < questionLibrary.getLength()) {
+                updateQuestion();
+            }
+        } );
+
+        // after click on the second button
+        buttonChoice2.setOnClickListener( view12 -> {
+            if (buttonChoice2.getText() == answer) {
+                score = score + 1;
+            }
+
+            if (questionNumber == questionLibrary.getLength()) {
+                if(score-1 < 3){
+                    updateScore( "low" );
+                }
+                else if (score-1 > 5){
+                    updateScore( "high" );
+                }
+                else {
+                    updateScore( "medium" );
+                }
+            }
+            if (questionNumber < questionLibrary.getLength()) {
+                updateQuestion();
+            }
+        } );
+        return view;
+    }
+    public void updateQuestion () {
+        questionView.setText( questionLibrary.getQuestion( questionNumber ) );
+        buttonChoice1.setText( questionLibrary.getChoice1( questionNumber ) );
+        buttonChoice2.setText( questionLibrary.getChoice2( questionNumber ) );
+        answer = questionLibrary.getCorrectAnswer( questionNumber );
+        questionNumber++;
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void updateScore (String point){
+        questionView.setText( "The chance of you having Covid-19 is " + point );
+        buttonChoice1.setText( "Try Again" );
+        buttonChoice2.setVisibility( View.GONE );
     }
 }
---
+++ app/src/main/java/com/example/covidapp/QuestionLibrary.java
@@ -0,0 +1,48 @@
+package com.example.covidapp;
+
+public class QuestionLibrary {
+    private final String[] questions = {
+            "1. Have you experienced any symptoms of COVID-19? \n\n" +
+                    "(ex. sore throat, fever over 100, cough, headache, shortness of breath or difficulty breathing, fatigue, " +
+                    "vomiting, body aches, loss of taste or smell.)",
+            "2. Have someone in your house experienced any symptoms of COVID-19?  \n\n" +
+                    "(ex. sore throat, fever over 100, cough, headache, shortness of breath or difficulty breathing, " +
+                    "fatigue, vomiting, body aches, loss of taste or smell.)",
+            "3. Have you been asked to quarantine or been exposed to a person who has is confirmed positive for COVID-19 in the past 2 weeks?",
+            "4. Have someone in your home been asked to quarantine or been exposed to a person who has is confirmed positive for COVID-19 in the past 2 weeks?",
+            "5. Have you or someone in your house traveled outside of the United States in the past week?",
+            "6. Have you or someone in your house recently traveled to areas with a high risk of COVID-19?",
+            "7. Do you feel like you are at a higher risk for contracting COVID-19?"
+    };
+
+    private final String[][] choices = {
+            {"yes", "no"},
+            {"yes", "no"},
+            {"yes", "no"},
+            {"yes", "no"},
+            {"yes", "no"},
+            {"yes", "no"},
+            {"yes", "no"}
+    };
+
+    public int getLength(){ return questions.length;}
+
+
+    private final String[] correctAnswers = {"yes", "yes", "yes", "yes", "yes", "yes", "yes"};
+
+    public String getQuestion(int a){
+        return questions[a];
+    }
+
+    public String getChoice1(int a){
+        return choices[a][0];
+    }
+
+    public String getChoice2(int a){
+        return choices[a][1];
+    }
+
+    public String getCorrectAnswer(int a){
+        return correctAnswers[a];
+    }
+}
